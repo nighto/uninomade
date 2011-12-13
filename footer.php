8f927ce96d8f44f852e3602eaa3db0573874417c
Arlindo Pereira <nighto@nighto.net>
2011-12-13 10:55:04 -0800

    Front page almost done.

--- footer.php
+++ footer.php
@@ -18,8 +18,7 @@
 	get_sidebar( 'footer' );
 ?>
 
-			<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-			<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
+	<p>Copyleft: Este site está sobre os termos da Licença da Arte Livre 1.3, com exceção dos conteúdos indicados com licença própria.</p>
 
 <?php
 	/* Always have wp_footer() just before the closing </body>
